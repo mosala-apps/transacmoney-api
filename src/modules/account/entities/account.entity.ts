f40706f037401fc1ef4b540c5bad393e99875a8b
rkota(Rosaire Kota) <rosaire@kinshasadigital.com>
2023-06-05 00:56:31 +0100

    fix: resolve date issues

--- src/modules/account/entities/account.entity.ts
+++ src/modules/account/entities/account.entity.ts
@@ -1,16 +1,23 @@
-import { Column, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 import { TimesTampEntity } from '~/ORM/base-entities/times-tamp/times-tamp.entity';
 import { Agency } from '~/modules/agency/entities/agency.entity';
 import { SubAgency } from '~/modules/sub-agency/entities/sub-agency.entity';
 
+@Entity('accounts')
 export class Account extends TimesTampEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
-  accountNumber: BigInteger;
+  @Column({ type: 'bigint' })
+  accountNumber: number;
 
-  @Column({ type: 'datetime', default: new Date() })
+  @CreateDateColumn()
   openingDate: Date;
 
   @OneToOne(() => Agency, (agency) => agency.account)
