8c1c9cf444100ce32c74ae7f7c1f7584dbfc315a
rkota(Rosaire Kota) <rosaire@kinshasadigital.com>
2023-06-05 01:02:29 +0100

    feat: add AgencyRepository

---
+++ src/modules/agency/repository/agency.repository.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+import { DataSource, Repository } from 'typeorm';
+import { Agency } from '../entities/agency.entity';
+
+@Injectable()
+export class AgencyRepository extends Repository<Agency> {
+  constructor(dataSource: DataSource) {
+    super(Agency, dataSource.createEntityManager());
+  }
+}
