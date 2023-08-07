9790b960dd92ac701cad718521ead5c5d417f37d
Méshak Ahoka <59387295+Meshak-okonda@users.noreply.github.com>
2023-08-07 18:08:20 +0100

    Create commision.controller.ts

---
+++ src/modules/commision/commision.controller.ts
@@ -0,0 +1,31 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { CommisionService } from './commision.service';
+import { CreateCommisionDto } from './dto/create-commision.dto';
+import { UpdateCommisionDto } from './dto/update-commision.dto';
+import { CommisionInfoDto } from './dto/commsion-info.dto';
+
+@Controller('commision')
+export class CommisionController {
+  constructor(private readonly commisionService: CommisionService) {}
+
+  @Get()
+  findAll() {
+    return this.commisionService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    return this.commisionService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: number, @Body('commision') updateCommisionDto: UpdateCommisionDto) {
+    return this.commisionService.update(+id, updateCommisionDto);
+  }
+
+  @Get('/resume')
+  resume(@Body('amount') amount: number, @Body("code") code: CommisionInfoDto) {
+    return this.commisionService.calculCommsion(amount, code);
+  }
+
+}
