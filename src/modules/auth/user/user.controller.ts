d68a136c2550c260d7e05080c46a2bc6bb663929
graciusyengo <yengogracius1996@gmail.com>
2024-10-14 01:05:35 +0200

    Add @ApiOperation decorator with summary

--- src/modules/auth/user/user.controller.ts
+++ src/modules/auth/user/user.controller.ts
@@ -14,31 +14,40 @@ import { User } from './entities/user.entity';
 import { UserCredentialsDto } from './dto/login-user.dto';
 import { ResetPasswordDto } from './dto/reset-password.dto';
 import { ForgotPasswordDto } from './dto/forgot-password.dto';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('auth')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @ApiOperation({ summary: 'Creation compte utilisateur' })
   @Post('register')
   create(@Body() createUserDto: RegisterUserDto): Promise<Partial<User>> {
     return this.userService.register(createUserDto);
   }
 
+  @ApiOperation({ summary: `Authentification de l'utilisateur` })
   @Post('login')
   async login(
     @Body() userCredentialsDto: UserCredentialsDto,
   ): Promise<Partial<User>> {
     return await this.userService.login(userCredentialsDto);
   }
+
+  @ApiOperation({ summary: `Mot de passe de l'utilisateur oublier` })
   @Post('forgot-password')
   async forgotPassword(@Body() forgotPasswordDto: ForgotPasswordDto) {
     const { email } = forgotPasswordDto;
     return await this.userService.forgotPassword(email);
   }
+
+  @ApiOperation({ summary: `Renitialisation ;ot de passe utilisateur` })
   @Post('reset-password')
   async resetPassword(@Body() resetPasswordDto: ResetPasswordDto) {
     return await this.userService.resetPassword(resetPasswordDto);
   }
+
+  @ApiOperation({ summary: `Obtenir la liste de tous les utilisateurs` })
   @Get()
   findAll() {
     return this.userService.findAll();
@@ -54,6 +63,8 @@ export class UserController {
     return this.userService.update(+id, updateUserDto);
   }
 
+  @ApiOperation({ summary: `Obtenir la liste de tous les utilisateurs` })
+  @Get()
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.userService.remove(+id);
--- src/modules/category/category.controller.ts
+++ src/modules/category/category.controller.ts
@@ -2,16 +2,20 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { CategoryService } from './category.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('category')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
 
+
+  @ApiOperation({ summary: `Creation d'une categorie` })
   @Post()
   create(@Body() createCategoryDto: CreateCategoryDto) {
     return this.categoryService.create(createCategoryDto);
   }
 
+  @ApiOperation({ summary: `Obtenir la liste de toutes les categories` })
   @Get()
   findAll() {
     return this.categoryService.findAll();
@@ -22,11 +26,15 @@ export class CategoryController {
     return this.categoryService.findOne(+id);
   }
 
+
+  @ApiOperation({ summary: `Modification d'une categorie`})
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateCategoryDto: UpdateCategoryDto) {
     return this.categoryService.update(+id, updateCategoryDto);
   }
 
+
+  @ApiOperation({ summary: `Suppression d'une categorie par son id`})
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.categoryService.remove(+id);
