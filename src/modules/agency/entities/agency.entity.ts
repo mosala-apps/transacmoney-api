261d5680637c52446bdf2445dc1f8af74b0dc509
rkota <rosairekota@anicns.gouv.cd>
2024-10-22 01:26:39 +0100

    refactor: update Agency Entity columns

--- src/modules/agency/entities/agency.entity.ts
+++ src/modules/agency/entities/agency.entity.ts
@@ -1,8 +1,16 @@
-import { Column, Entity, JoinColumn, OneToMany, OneToOne } from 'typeorm';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  OneToOne,
+} from 'typeorm';
 import { BaseAgencyEntity } from '~/ORM/base-entities/base-agency.entity';
 import { AgencyTypeEnum } from '~/enums/agency-type.enum';
 import { Account } from '~/modules/account/entities/account.entity';
 import { User } from '~/modules/auth/user/entities/user.entity';
+import { City } from '~/modules/city/entities/city.entity';
 
 @Entity('agencies')
 export class Agency extends BaseAgencyEntity {
@@ -21,9 +29,16 @@ export class Agency extends BaseAgencyEntity {
   @JoinColumn({ name: 'users_agency' })
   usersAgency: User[];
 
-  @OneToOne(() => User, (user) => user.agencyResponsible)
+  @OneToOne(() => User, (user) => user.agencyResponsible, {
+    onDelete: 'CASCADE',
+    nullable: true,
+    eager: true,
+  })
   @JoinColumn({ name: 'responsible_id' })
   responsible: User;
 
-  
+  @ManyToOne(() => City, (city) => city.agencies, {
+    nullable: true,
+  })
+  city: City;
 }
